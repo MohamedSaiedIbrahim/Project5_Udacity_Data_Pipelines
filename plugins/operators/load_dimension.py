e833e93da05af1a4205761ac88b49958ba20830b
Mohamed_Saied <eng.mohamedsaid2006@gmail.com>
2023-01-09 22:43:07 +0200

    first commit

---
+++ plugins/operators/load_dimension.py
@@ -0,0 +1,38 @@
+from airflow.hooks.postgres_hook import PostgresHook
+from airflow.models import BaseOperator
+from airflow.utils.decorators import apply_defaults
+
+class LoadDimensionOperator(BaseOperator):
+
+    ui_color = '#80BD9E'
+
+    @apply_defaults
+    def __init__(self,
+                 redshift_conn_id="",
+                 table="",
+                 select_sql="",
+                 append_insert=False,
+                 primary_key="",
+                 *args, **kwargs):
+        
+        super(LoadDimensionOperator, self).__init__(*args, **kwargs)
+        self.redshift_conn_id = redshift_conn_id
+        self.table = table
+        self.select_sql = select_sql
+        self.append_insert = append_insert
+        self.primary_key = primary_key
+    def execute(self, context):
+        
+        self.log.info("Getting credentials")
+        redshift_hook = PostgresHook(postgres_conn_id=self.redshift_conn_id)
+        
+		table_insert_sql = f"""
+			insert into {self.table}
+			{self.select_sql}
+		"""
+		
+		self.log.info("Clearing data from dimension table in Redshift")
+		redshift_hook.run(f"TRUNCATE TABLE {self.table};")
+
+        self.log.info("Loading data into dimension table in Redshift")
+        redshift_hook.run(table_insert_sql)
